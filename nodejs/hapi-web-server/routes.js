543005fbe826bcd2e480fa2eac1fcda29cd0fe10
vannyezha <vannyezhaa@gmail.com>
2022-11-04 13:08:51 +0700

    hapi framework

---
+++ nodejs/hapi-web-server/routes.js
@@ -0,0 +1,61 @@
+
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Homepage';
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+           const { name = "stranger" } = request.params;
+           const { lang } = request.query;
+ 
+            if(lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+           return `Hello, ${name}!`;
+       },
+    },
+    {
+        method: 'POST',
+        path: '/login',
+        handler: (request, h) => {
+            const { username, password } = request.payload;
+            return `Welcome ${username}, your password is ${password}!`;
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+ 
+module.exports = routes;
